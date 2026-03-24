2d2b360c7932bab5acbfbe801c50e8e3e6498177
June Kim <kimjune01@gmail.com>
2026-03-23 19:42:32 -0700

    Add sitemap, semiring table, licenses-are-functors post fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), sitemap()],
   site: 'https://june.kim',
   base: '/reading/',
   vite: {
